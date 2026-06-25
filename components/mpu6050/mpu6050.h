e84d3db01f6beca584e8482be13126ff2062d8da
TarheelGrad1998 <47931551+TarheelGrad1998@users.noreply.github.com>
2026-06-25 10:34:13 -0400

    Update mpu6050.h

--- components/mpu6050/mpu6050.h
+++ components/mpu6050/mpu6050.h
@@ -13,22 +13,14 @@ class MPU6050Component : public PollingComponent, public i2c::I2CDevice {
 
   void update() override;
 
-  void set_accel_x_sensor(sensor::Sensor *accel_x_sensor) { accel_x_sensor_ = accel_x_sensor; }
-  void set_accel_y_sensor(sensor::Sensor *accel_y_sensor) { accel_y_sensor_ = accel_y_sensor; }
-  void set_accel_z_sensor(sensor::Sensor *accel_z_sensor) { accel_z_sensor_ = accel_z_sensor; }
+  void set_accel_sensor(sensor::Sensor *accel_sensor) { accel_sensor_ = accel_sensor; }
   void set_temperature_sensor(sensor::Sensor *temperature_sensor) { temperature_sensor_ = temperature_sensor; }
-  void set_gyro_x_sensor(sensor::Sensor *gyro_x_sensor) { gyro_x_sensor_ = gyro_x_sensor; }
-  void set_gyro_y_sensor(sensor::Sensor *gyro_y_sensor) { gyro_y_sensor_ = gyro_y_sensor; }
-  void set_gyro_z_sensor(sensor::Sensor *gyro_z_sensor) { gyro_z_sensor_ = gyro_z_sensor; }
+  void set_gyro_sensor(sensor::Sensor *gyro_sensor) { gyro_sensor_ = gyro_sensor; }
 
  protected:
-  sensor::Sensor *accel_x_sensor_{nullptr};
-  sensor::Sensor *accel_y_sensor_{nullptr};
-  sensor::Sensor *accel_z_sensor_{nullptr};
+  sensor::Sensor *accel_sensor_{nullptr};
   sensor::Sensor *temperature_sensor_{nullptr};
-  sensor::Sensor *gyro_x_sensor_{nullptr};
-  sensor::Sensor *gyro_y_sensor_{nullptr};
-  sensor::Sensor *gyro_z_sensor_{nullptr};
+  sensor::Sensor *gyro_sensor_{nullptr};
 };
 
 }  // namespace esphome::mpu6050
